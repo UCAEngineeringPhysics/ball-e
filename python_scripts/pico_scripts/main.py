f764ffff5d2881ad609d7cb1e828bb95774351ea
ericlejune <elejune@cub.uca.edu>
2025-10-30 19:46:33 -0500

    Update main.py

--- python_scripts/pico_scripts/main.py
+++ python_scripts/pico_scripts/main.py
@@ -5,7 +5,8 @@ Rename this script to main.py, then upload to the pico board.
 import sys
 import select
 from diff_drive_controller import DiffDriveController
-from machine import WDT, freq, reset
+from machine import freq, reset
+from utime import ticks_us
 
 # SETUP
 # Overclock
@@ -18,35 +19,24 @@ balle = DiffDriveController(
 cmd_vel_listener = select.poll()
 cmd_vel_listener.register(sys.stdin, select.POLLIN)
 event = cmd_vel_listener.poll()
-# Config watchdog timer
-wdt = WDT(timeout=500)  # ms
+target_lin_vel, target_ang_vel = 0.0, 0.0
+tic = ticks_us()
 
 # LOOP
-try:
-    while True:
-        target_lin_vel, target_ang_vel = 0.0, 0.0
-        for msg, _ in event:
-            if msg:
-                wdt.feed()
-                buffer = msg.readline().rstrip().split(",")
-                # print(f"{balle.lin_vel},{balle.ang_vel}")
-                if len(buffer) == 2:
-                    try:
-                        target_lin_vel = float(buffer[0])
-                        target_ang_vel = float(buffer[1])
-                        balle.set_vel(target_lin_vel, target_ang_vel)
-                    except ValueError:
-                        balle.set_vel(0.0, 0.0)
-                        # print("ValueError!")  # debug
-                        # reset()
-            else:
-                # print("No message received")  # debug
-                balle.set_vel(0.0, 0.0)
-                # reset()
+while True:
+    for msg, _ in event:
+        buffer = msg.readline().strip().split(",")
+        # print(f"{balle.lin_vel},{balle.ang_vel}")
+        if len(buffer) == 2:
+            target_lin_vel = float(buffer[0])
+            target_ang_vel = float(buffer[1])
+            balle.set_vel(target_lin_vel, target_ang_vel)
+    toc = ticks_us()
+    if toc - tic >= 10000:
+        meas_lin_vel, meas_ang_vel = balle.get_vel()
+        out_msg = f"{meas_lin_vel}, {meas_ang_vel}\n"
+#         out_msg = "PICO\n"
+        sys.stdout.write(out_msg)
+        tic = ticks_us()
+        
 
-except Exception as e:
-    # print('Pico reset')  # debug
-    reset()
-finally:
-    # print('Pico reset')  # debug
-    reset()
