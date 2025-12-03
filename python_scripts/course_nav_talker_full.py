096fe964e5a446d0457c70feb17efab1010da08d
ericlejune <elejune@cub.uca.edu>
2025-12-02 23:30:47 -0600

    Update course_nav_talker_full.py

--- python_scripts/course_nav_talker_full.py
+++ python_scripts/course_nav_talker_full.py
@@ -104,23 +104,23 @@ def app_callback(pad, info, user_data):
         elif user_data.arm_state == "idle":
             pass         
         
-    # elif user_data.mode == "drop":
-    #     user_data.latest_msg = "0.0, 0.0, 0, 0\n".encode('utf-8')
-    #     if user_data.arm_state == "lower":
-    #         user_data.latest_msg = "0.0, 0.0, 1000, 0\n".encode('utf-8')
-    #         user_data.picker_counter += 1
-    #         if user_data.picker_counter >= 180:
-    #             user_data.arm_state = "open"
-    #             user_data.picker_counter = 0          
+    elif user_data.mode == "drop":
+        user_data.latest_msg = "0.0, 0.0, 0, 0\n".encode('utf-8')
+        if user_data.arm_state == "lower":
+            user_data.latest_msg = "0.0, 0.0, 1000, 0\n".encode('utf-8')
+            user_data.picker_counter += 1
+            if user_data.picker_counter >= 100:
+                user_data.arm_state = "open"
+                user_data.picker_counter = 0          
                 
-    #     elif user_data.arm_state == "open":
-    #         user_data.latest_msg = "0.0, 0.0, 0, -1000\n".encode('utf-8')
-    #         user_data.picker_counter += 1
-    #         if user_data.picker_counter >= 250:
-    #             #Return both arm and claw to neutral
-    #             user_data.arm_state = "idle"
-    #             user_data.picker_counter = 0
-#                           
+        elif user_data.arm_state == "open":
+            user_data.latest_msg = "0.0, 0.0, 0, -1000\n".encode('utf-8')
+            user_data.picker_counter += 1
+            if user_data.picker_counter >= 200:
+                #Return both arm and claw to neutral
+                user_data.arm_state = "idle"
+                user_data.picker_counter = 0
+                          
             
     elif user_data.mode == "fixed":
         user_data.latest_msg = "0.2, 0.0, 0, 0\n".encode('utf-8')
@@ -167,15 +167,15 @@ def app_callback(pad, info, user_data):
                 string_to_print += (f"Detection: ID: {track_id} Label: {label} Confidence: {confidence:.2f}\n")
                 string_to_print += (f"X Center: {(bbox.xmin() + bbox.xmax()) / 2}, Y Center: {(bbox.ymin() + bbox.ymax()) / 2}\n")
                 # if Z > 2.4:
-                if user_data.distance >= 4:
-                    if (bbox.xmin() + bbox.xmax()) / 2 < 0.4:
+                if user_data.distance >= 2.4:
+                    if (bbox.xmin() + bbox.xmax()) / 2 < 4:
                         user_data.latest_msg = "0.2, 0.5, 0, 0\n".encode('utf-8')
                     elif (bbox.xmin() + bbox.xmax()) / 2 > 0.7:
                         user_data.latest_msg = "0.2, -0.5, 0, 0\n".encode('utf-8')
                     else:
                         user_data.latest_msg = "0.2, 0.0, 0, 0\n".encode('utf-8')
                 # elif Z <= 2.4 and Z > 1.0:
-                elif 2.5 < user_data.distance <= 4:
+                elif 2 < user_data.distance <= 4:
                     if (bbox.xmin() + bbox.xmax()) / 2 < 0.4:
                         user_data.latest_msg = "0.1, 0.5, 0, 0\n".encode("utf-8")
                     elif (bbox.xmin() + bbox.xmax()) / 2 > 0.7:
@@ -185,8 +185,8 @@ def app_callback(pad, info, user_data):
                 else:
                     # Stop wheels and start arm sequence only once
                     user_data.latest_msg = "0.0, 0.0, 0, 0\n".encode()
-                    #user_data.arm_state = "lower"
-                    #user_data.mode = "drop"
+                    user_data.arm_state = "lower"
+                    user_data.mode = "drop"
         else:  # no detection
             user_data.latest_msg = "0.0, 0.0, 0, 0\n".encode()          
                   
