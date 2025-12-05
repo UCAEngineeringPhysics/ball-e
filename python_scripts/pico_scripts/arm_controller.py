3a74e2dbcd7ef4f86bc7bfa4f390bf5d9fa93c3f
elizabeth-1336 <elizabeth.lopez423el@gmail.com>
2025-12-04 19:08:36 -0600

    Refactor arm_controller.py to use ArmController class
    Refactor arm controller script to include class methods for controlling the arm and claw servos, and add example usage.

--- python_scripts/pico_scripts/arm_controller.py
+++ python_scripts/pico_scripts/arm_controller.py
@@ -1,52 +1,89 @@
-"""
-Rename this script to main.py, then upload to the pico board.
-"""
+from machine import Pin, PWM
+from time import sleep
 
-import sys
-import select
-from diff_drive_controller import DiffDriveController
-from arm_controller import ArmController
-from machine import freq, reset
-from utime import ticks_us
+SHOULDER_NEUTRAL = 1_400_000
 
-# SETUP
-# Overclock
-freq(200_000_000)  # Pico 2 original: 150_000_000
-# Instantiate robot
-ddc = DiffDriveController(
-    left_ids=((6, 7, 8), (11, 10)), right_ids=((2, 3, 4), (21, 20))
-)
-arm = ArmController(12, 13, 14)
-# Create a poll to receive messages from host machine
-cmd_vel_listener = select.poll()
-cmd_vel_listener.register(sys.stdin, select.POLLIN)
-event = cmd_vel_listener.poll()
-target_lin_vel, target_ang_vel = 0.0, 0.0
-tic = ticks_us()
+CLAW_NEUTRAL = 1_800_000
 
-# LOOP
-while True:
-    for msg, _ in event:
-        buffer = msg.readline().strip().split(",")
-        # print(f"{balle.lin_vel},{balle.ang_vel}")
-        if len(buffer) == 4:
-            target_lin_vel = float(buffer[0])
-            target_ang_vel = float(buffer[1])
-            sho_vel = int(buffer[2])
-            cla_vel = int(buffer[3])
-            ddc.set_vels(target_lin_vel, target_ang_vel)
-            arm.lower_claw(sho_vel)
-            arm.close_claw(cla_vel)
-    toc = ticks_us()
-    if toc - tic >= 10000:
-        meas_lin_vel, meas_ang_vel = ddc.get_vels()
-        shoulder_duty_a = arm.shoulder_duty_a
-        shoulder_duty_b = arm.shoulder_duty_b
 
-        claw_duty = arm.claw_duty
-        out_msg = f"{meas_lin_vel}, {meas_ang_vel}\n"
-        #         out_msg = "PICO\n"
-        sys.stdout.write(out_msg)
-        tic = ticks_us()
+class ArmController:
+    def __init__(self, claw_pin, arm_pin_a, arm_pin_b):
+        self.claw_servo = PWM(Pin(claw_pin))
+        self.shoulder_servo_a = PWM(Pin(arm_pin_a))
+        self.shoulder_servo_b = PWM(Pin(arm_pin_b))
+        self.claw_servo.freq(50)
+        self.shoulder_servo_a.freq(50)
+        self.shoulder_servo_b.freq(50)
+
+        
+        
+        # Set initial positions
+        self.set_neutral()
+
+    def set_neutral(self):
+        # Return to rest positions
+        self.shoulder_servo_a.duty_ns(SHOULDER_NEUTRAL)
+        self.shoulder_servo_b.duty_ns(SHOULDER_NEUTRAL)
+        self.claw_servo.duty_ns(CLAW_NEUTRAL)
+        self.shoulder_duty_a = SHOULDER_NEUTRAL
+        self.shoulder_duty_b = SHOULDER_NEUTRAL
+
+        self.claw_duty = CLAW_NEUTRAL
+
+    def lower_claw(self, dc_inc=0):  # Lower arm
+        assert -50_000 <= dc_inc <= 50_000
+        self.shoulder_duty_a += dc_inc
+        self.shoulder_duty_b -= dc_inc
+
+        if self.shoulder_duty_a >= 2_400_000:
+            self.shoulder_duty_a = 2_400_000
+            self.shoulder_duty_b = 700_000
+    
+            
+        elif self.shoulder_duty_a <= 700_000:
+            self.shoulder_duty_a = 700_000
+            self.shoulder_duty_b = 2_400_000
+
+        self.shoulder_servo_a.duty_ns(self.shoulder_duty_a)
+        self.shoulder_servo_b.duty_ns(self.shoulder_duty_b)
+
+
+    def close_claw(self, dc_inc=0):  # Close claw
+        assert -50_000 <= dc_inc <= 50_000
+        self.claw_duty += dc_inc
+        if self.claw_duty >= 2_050_000:
+            self.claw_duty = 2_050_000
+        elif self.claw_duty <= 1_550_000:
+            self.claw_duty = 1_550_000
+        self.claw_servo.duty_ns(self.claw_duty)
+
+
+# Example usage
+if __name__ == "__main__":
+    from utime import sleep
+
+    sleep(1)
+    ac = ArmController(12, 13, 14)
+    for _ in range(40):
+        ac.close_claw(10_000)
+        sleep(0.1)
+        print(f"Closing claw duty cycle: {ac.claw_duty}")
+#     for _ in range(20):
+#         ac.close_claw(-20_000)
+#         sleep(0.1)
+#         print(f"Opening claw duty cycle: {ac.claw_duty}")
+# 
+#     ac.set_neutral()
+#     sleep(1)
+#     print("arm set to neutral")
+# 
+#     for _ in range(20):
+#         ac.lower_claw(20_000)
+#         sleep(0.1)
+#         #print(f"Lowering claw duty cycle: {ac.shoulder_duty}")
+#     for _ in range(20):
+#         ac.lower_claw(-20_000)
+#         sleep(0.1)
+        #print(f"Lifting claw duty cycle: {ac.shoulder_duty}")
 
 
