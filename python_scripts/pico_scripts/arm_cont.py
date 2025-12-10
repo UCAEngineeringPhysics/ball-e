fb89b1d763c3a43945b8024f85e4c0bd547c29d8
Caleb-M-Y <cyoung24@cub.uca.edu>
2025-12-10 01:00:00 -0600

    fuck

--- python_scripts/pico_scripts/arm_cont.py
+++ python_scripts/pico_scripts/arm_cont.py
@@ -2,8 +2,7 @@ from machine import Pin, PWM
 from time import sleep
 
 SHOULDER_NEUTRAL = 1_400_000  # Starting position for both arm servos
-CLAW_NEUTRAL = 1_950_000  # Starting position for claw (FIXED from 1_800_000)
-
+CLAW_NEUTRAL = 1_950_000  # Starting position for claw
 
 class ArmController:
     def __init__(self, claw_pin, arm_pin_left, arm_pin_right):
@@ -19,9 +18,14 @@ class ArmController:
         self.shoulder_servo_right.freq(50)
         
         # Set initial positions
+        self.shoulder_duty_left = SHOULDER_NEUTRAL
+        self.shoulder_duty_right = SHOULDER_NEUTRAL
+        self.claw_duty = CLAW_NEUTRAL
+        
         self.set_neutral()
         
     def set_neutral(self):
+        """Reset all servos to neutral position"""
         self.shoulder_duty_left = SHOULDER_NEUTRAL
         self.shoulder_duty_right = SHOULDER_NEUTRAL
         self.claw_duty = CLAW_NEUTRAL
@@ -31,35 +35,38 @@ class ArmController:
         self.claw_servo.duty_ns(self.claw_duty)
         
 
-    def lower_claw(self, dc_inc=0):  # Control arm height
+    def lower_claw(self, dc_inc=0):
         """
-        Positive dc_inc: RAISE arm (increase right, decrease left)
-        Negative dc_inc: LOWER arm (decrease right, increase left)
+        Control arm height (shoulder servos)
+        Positive dc_inc: LOWER arm (increase left, decrease right)
+        Negative dc_inc: RAISE arm (decrease left, increase right)
         """
         assert -50_000 <= dc_inc <= 50_000
         
-        # To RAISE: increase right servo, decrease left servo
-        self.shoulder_duty_right += dc_inc
-        self.shoulder_duty_left -= dc_inc
+        # When dc_inc is NEGATIVE (lift button): decrease left, increase right → RAISE
+        # When dc_inc is POSITIVE (lower button): increase left, decrease right → LOWER
+        self.shoulder_duty_left += dc_inc
+        self.shoulder_duty_right -= dc_inc
 
+        # Limit left servo
+        if self.shoulder_duty_left >= 2_400_000:
+            self.shoulder_duty_left = 2_400_000
+        elif self.shoulder_duty_left <= 700_000:
+            self.shoulder_duty_left = 700_000
+            
         # Limit right servo
         if self.shoulder_duty_right >= 2_400_000:
             self.shoulder_duty_right = 2_400_000
         elif self.shoulder_duty_right <= 700_000:
             self.shoulder_duty_right = 700_000
-            
-        # Limit left servo (opposite direction)
-        if self.shoulder_duty_left >= 2_400_000:
-            self.shoulder_duty_left = 2_400_000
-        elif self.shoulder_duty_left <= 700_000:
-            self.shoulder_duty_left = 700_000
 
         self.shoulder_servo_left.duty_ns(self.shoulder_duty_left)
         self.shoulder_servo_right.duty_ns(self.shoulder_duty_right)
 
 
-    def close_claw(self, dc_inc=0):  # Control claw open/close
+    def close_claw(self, dc_inc=0):
         """
+        Control claw open/close
         Positive dc_inc: OPEN claw (increase duty)
         Negative dc_inc: CLOSE claw (decrease duty)
         """
