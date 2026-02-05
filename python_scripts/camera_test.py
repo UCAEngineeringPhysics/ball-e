899978754338be10225343b0c2385bc387daf388
ericlejune <elejune@cub.uca.edu>
2026-02-04 21:00:57 -0600

    Update camera_test.py

--- python_scripts/camera_test.py
+++ python_scripts/camera_test.py
@@ -12,6 +12,7 @@ import serial
 
 # Local application-specific imports
 import hailo
+from hailo_apps.hailo_app_python.core.common.buffer_utils import get_caps_from_pad, get_numpy_from_buffer
 from hailo_apps.hailo_app_python.core.gstreamer.gstreamer_app import app_callback_class
 from hailo_apps.hailo_app_python.apps.detection_simple.detection_pipeline_simple import GStreamerDetectionApp
 # endregion imports
@@ -24,14 +25,18 @@ class user_app_callback_class(app_callback_class):
         self.messenger = serial.Serial(port='/dev/ttyACM0', baudrate=115200)  # New variable example
         print(f"Messenger initiated at: {self.messenger.name}\n")
         # Shared variable for latest message
-        self.latest_msg = "0.0, 0.0\n".encode('utf-8')
+        self.latest_msg = "0.0, 0.0, 0, 0, 0\n".encode('utf-8')
         
         # Start Pico update thread
         self.pico_thread = threading.Thread(target=self.send_msg, daemon=True)
         self.pico_thread.start()
         self.vel =0
-
         
+        self.mode = "detect"
+        self.arm_state = "idle"
+        self.fixed_travel_counter = 0
+        self.picker_counter = 0
+
     def send_msg(self):
         """Continuously send the latest message to the Pico."""
         while True:
@@ -52,45 +57,200 @@ def app_callback(pad, info, user_data):
     if buffer is None:  # Check if the buffer is valid
         return Gst.PadProbeReturn.OK
     
-    detection_count = 0
-    roi = hailo.get_roi_from_buffer(buffer)
-    detections = roi.get_objects_typed(hailo.HAILO_DETECTION)
+    # Using the user_data to count the number of frames
+    user_data.increment()
+    string_to_print = f"Frame count: {user_data.get_count()}\n"
+        # Get resolution size
+    (
+        caps_string,
+        frame_width,
+        frame_height,
+    ) = get_caps_from_pad(pad)
+    user_data.frame_width = frame_width
+    user_data.frame_height = frame_height
+   
+    if user_data.mode == "pause":
+            user_data.latest_msg = "0.0, 0.0, 0, 0, 0\n".encode('utf-8')
+                 
+    elif user_data.mode == "pick":
+            # Always reset arm state when entering pick
+        if user_data.arm_state == "idle":
+            user_data.arm_state = "lower"
+            user_data.picker_counter = 0
+            
+        user_data.latest_msg = "0.0, 0.0, 0, 0, 0\n".encode('utf-8')
+        if user_data.arm_state == "lower":
+            user_data.latest_msg = "0.0, 0.0, 3000, 0, 0\n".encode('utf-8')
+            user_data.picker_counter += 1
+            if user_data.picker_counter >= 210:
+                user_data.arm_state = "close"
+                user_data.picker_counter = 0
+                
+        elif user_data.arm_state == "close":
+            user_data.latest_msg = "0.0, 0.0, 0, 3000, 0\n".encode('utf-8')
+            user_data.picker_counter += 1
+            if user_data.picker_counter >= 70:
+                user_data.arm_state = "raise"
+                user_data.picker_counter = 0
+                
+        elif user_data.arm_state == "raise":
+            user_data.latest_msg = "0.0, 0.0, -3000, 0, 0\n".encode('utf-8')
+            user_data.picker_counter += 1
+            if user_data.picker_counter >= 180:
+                user_data.latest_msg = "0.0, 0.0, 0, 0, 0\n".encode('utf-8')
+                user_data.mode = "detect_bucket"
+                user_data.picker_counter = 0
+        # idle = normal driving
+        elif user_data.arm_state == "idle":
+            pass         
 
-           
-    for detection in detections:
-#for detection in hailo.get_roi_from_buffer(buffer).get_objects_typed(hailo.HAILO_DETECTION):  # Get the detections from the buffer & Parse the detections
+    elif user_data.mode == "drop":
+            user_data.latest_msg = "0.0, 0.0, 0, 0, 0\n".encode('utf-8')
+            if user_data.arm_state == "lower":
+                user_data.latest_msg = "0.0, 0.0, 3000, 0, 0\n".encode('utf-8')
+                user_data.picker_counter += 1
+                if user_data.picker_counter >= 50:
+                    user_data.arm_state = "open"
+                    user_data.picker_counter = 0          
+                    
+            elif user_data.arm_state == "open":
+                user_data.latest_msg = "0.0, 0.0, 0, -3000, 0\n".encode('utf-8')
+                if user_data.picker_counter >= 40:
+                    #Return both arm and claw to neutral
+                    user_data.latest_msg = "0.0, 0.0, 0, 0,10\n".encode('utf-8')
+                    user_data.arm_state = "idle" 
+                    user_data.picker_counter = 0  
+                    user_data.fixed_travel_counter = 0
+                    user_data.mode = "detect"
 
-        label = detection.get_label()
-        bbox = detection.get_bbox()
-        confidence = detection.get_confidence()
 
-        if "ball" in label:
-            # Get track ID
-            track_id = 0
-            track = detection.get_objects_typed(hailo.HAILO_UNIQUE_ID)
-            if len(track) == 1:
-                track_id = track[0].get_id()
-            string_to_print += (f"Detection: ID: {track_id} Label: {label} Confidence: {confidence:.2f}\n")
-            string_to_print += (f"X Center: {(bbox.xmin() + bbox.xmax()) / 2}, Y Center: {(bbox.ymin() + bbox.ymax()) / 2}\n")
-            if (bbox.xmin() + bbox.xmax()) / 2 < 0.3:
-                user_data.latest_msg = "0.4, 1.0\n".encode('utf-8')
-            elif (bbox.xmin() + bbox.xmax()) / 2 > 0.7:
-                user_data.latest_msg = "0.4, -1.0\n".encode('utf-8')
-            else:
-                user_data.latest_msg = "0.4, 0.0\n".encode('utf-8')
-            detection_count += 1
 
-            break
+    elif user_data.mode == "detect":
+
+        roi = hailo.get_roi_from_buffer(buffer)
+        detections = roi.get_objects_typed(hailo.HAILO_DETECTION)
+
+        if len(detections):            
+            for detection in detections:
+            #for detection in hailo.get_roi_from_buffer(buffer).get_objects_typed(hailo.HAILO_DETECTION):  # Get the detections from the buffer & Parse the detections
+
+                label = detection.get_label()
+                bbox = detection.get_bbox()
+                confidence = detection.get_confidence()
+
+            if "ball" in label:
+                # Get bounding box height in pixels
+                h_pixels = (bbox.ymax() - bbox.ymin()) * user_data.frame_height
+                # focal length in pixels
+                f_pixels = 3386.0
+                # Height of bucket
+                H_real = 0.381  # meters
+                # Distance from camera to bucket
+                # Z = (f_pixels * H_real) / h_pixels
+                user_data.distance = (f_pixels * H_real) / h_pixels
+
+                # Get track ID
+                user_data.vel = 0.4
+                track_id = 0
+                track = detection.get_objects_typed(hailo.HAILO_UNIQUE_ID)
+                if len(track) == 1:
+                    track_id = track[0].get_id()
+                string_to_print += (f"Detection: ID: {track_id} Label: {label} Confidence: {confidence:.2f}\n")
+                string_to_print += (f"X Center: {(bbox.xmin() + bbox.xmax()) / 2}, Y Center: {(bbox.ymin() + bbox.ymax()) / 2}\n")
+
+                # if Z > 5.0:
+                if user_data.distance >= 5.0:            
+                    if (bbox.xmin() + bbox.xmax()) / 2 < 0.3:
+                        user_data.latest_msg = "0.2, 0.5,0, 0, 0\n".encode('utf-8')
+                    elif (bbox.xmin() + bbox.xmax()) / 2 > 0.7:
+                        user_data.latest_msg = "0.2, -0.5,0, 0, 0\n".encode('utf-8')
+                    else:
+                        user_data.latest_msg = "0.2, 0.0,0, 0, 0\n".encode('utf-8')
+
+                # elif Z <= 3.5 and Z > 5.0:
+                elif 3.5 < user_data.distance <= 5.0:
+                    if (bbox.xmin() + bbox.xmax()) / 2 < 0.5:
+                        user_data.latest_msg = "0.1, 0.5, 0, 0, 0\n".encode("utf-8")
+                    elif (bbox.xmin() + bbox.xmax()) / 2 > 0.7:
+                        user_data.latest_msg = "0.1, -0.5, 0, 0, 0\n".encode("utf-8")
+                    else:
+                        user_data.latest_msg = "0.1, 0.0, 0, 0, 0\n".encode("utf-8")
+
+                else:
+                    # Stop wheels and start arm sequence only once
+                    user_data.latest_msg = "0.0, 0.0, 0, 0, 0\n".encode()
+                    user_data.arm_state = "lower"
+                    user_data.mode = "pick"
 
         # If no ball detected, gradually reduce velocity
         else:
-            user_data.vel = max(user_data.vel - 0.05, 0.0)
-            user_data.latest_msg = f"{user_data.vel}, 0.0\n".encode('utf-8')
+            user_data.latest_msg = "0.0, 0.0, 0, 0, 0\n".encode()
+
+    elif user_data.mode == "detect_bucket":
+            #Travel based on obj detection
+            # Get the detections from the buffer
+            roi = hailo.get_roi_from_buffer(buffer)
+            detections = roi.get_objects_typed(hailo.HAILO_DETECTION)
+
+            if len(detections):            
+                for detection in detections:
+                    label = detection.get_label()
+                    bbox = detection.get_bbox()
+                    confidence = detection.get_confidence()
+
+                if "bucket" not in label:   
+                    print("bucket not in label")         
+                    user_data.vel = max(user_data.vel - 0.05, 0.0)
+                    user_data.latest_msg = f"{user_data.vel}, 0.0\n".encode('utf-8')               
+                else:
+                    # Get bounding box height in pixels
+                    h_pixels = (bbox.ymax() - bbox.ymin()) * user_data.frame_height
+                    # focal length in pixels
+                    f_pixels = 3386.0
+                    # Height of bucket
+                    H_real = 0.381  # meters
+                    # Distance from camera to bucket
+                    # Z = (f_pixels * H_real) / h_pixels
+                    user_data.distance = (f_pixels * H_real) / h_pixels
+
+                    # Get track ID
+                    user_data.vel = 0.4
+                    track_id = 0
+                    track = detection.get_objects_typed(hailo.HAILO_UNIQUE_ID)
+                    if len(track) == 1:
+                        track_id = track[0].get_id()
+                    string_to_print += (f"Detection: ID: {track_id} Label: {label} Confidence: {confidence:.2f}\n")
+                    string_to_print += (f"X Center: {(bbox.xmin() + bbox.xmax()) / 2}, Y Center: {(bbox.ymin() + bbox.ymax()) / 2}\n")
+                    # if Z > 2.4:
+                    if user_data.distance >= 7.0:
+                        if (bbox.xmin() + bbox.xmax()) / 2 < 0.4:
+                            user_data.latest_msg = "0.2, 0.5, 0, 0, 0\n".encode('utf-8')
+                        elif (bbox.xmin() + bbox.xmax()) / 2 > 0.7:
+                            user_data.latest_msg = "0.2, -0.5, 0, 0, 0\n".encode('utf-8')
+                        else:
+                            user_data.latest_msg = "0.2, 0.0, 0, 0, 0\n".encode('utf-8')
+                    # elif Z <= 2.4 and Z > 1.0:
+                    elif 4.0 < user_data.distance <= 7.0:
+                        if (bbox.xmin() + bbox.xmax()) / 2 < 0.5:
+                            user_data.latest_msg = "0.1, 0.5, 0, 0, 0\n".encode("utf-8")
+                        elif (bbox.xmin() + bbox.xmax()) / 2 > 0.7:
+                            user_data.latest_msg = "0.1, -0.5, 0, 0, 0\n".encode("utf-8")
+                        else:
+                            user_data.latest_msg = "0.1, 0.0, 0, 0, 0\n".encode("utf-8")
+                    else:
+                        # Stop wheels and start arm sequence only once
+                        user_data.latest_msg = "0.0, 0.0, 0, 0, 0\n".encode()
+                        user_data.arm_state = "lower"
+                        user_data.mode = "drop"
+            else:  # no detection
+                user_data.latest_msg = "0.0, 0.0, 0, 0, 0\n".encode()          
+
+
 
     string_to_print += (f"Target velocity: {user_data.latest_msg}")
     print(string_to_print)
     return Gst.PadProbeReturn.OK
-    
+
 
 if __name__ == "__main__":
     user_data = user_app_callback_class()  # Create an instance of the user app callback class
